5061494c6dc942967322319d0add1de2d2cf5396
Manish Acharya <manuacharya54@gmail.com>
2025-07-12 04:18:52 +0545

    refactor: add docstrings, clean up imports, and update requirements
    - Added playful docstrings and comments to __init__.py and autostart.py
    - Cleaned up imports and comments in mouse_keyboard.py
    - Updated requirements.txt dependencies
    - Minor docstring tweak in command.py

--- desktop/features/__init__.py
+++ desktop/features/__init__.py
@@ -1,1 +1,22 @@
 # Features module init
+"""
+IDK increased overhead fr fr
+"""
+
+# from .battery import *
+# from .clipboard import *
+# from .command import *
+# from .file_transfer import *
+# from .mouse_keyboard import *
+# from .multimedia import *
+# from .notifications import *
+
+# __all__ = [
+#     "battery",
+#     "clipboard",
+#     "command",
+#     "file_transfer",
+#     "mouse_keyboard",
+#     "multimedia",
+#     "notifications",
+# ]
--- desktop/utils/autostart.py
+++ desktop/utils/autostart.py
@@ -1,1 +1,23 @@
-# OS autostart logic
+"""
+BLUE SCREEN OF DEATH LOGIC! GGS!
+"""
+
+
+
+
+# # OS autostart logic
+# import os
+# import sys
+# import subprocess
+# import plistlib
+# from pathlib import Path
+
+# class AutostartManager:
+#     def __init__(self, app_name, exec_path):
+#         self.app_name = app_name
+#         self.exec_path = exec_path
+#         self.platform = sys.platform
+
+#     def enable(self):
+#         if self.platform == 'win32':
+#             return self._enable_windows()
